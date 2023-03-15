c58f5c5507965df6972d0679f558228476839f7e
Firex86 <46250017+Firex86@users.noreply.github.com>
2023-03-15 16:06:17 +0200

    added first commit for index

--- webserverfunctions.js
+++ webserverfunctions.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080; //listens to port provided by webhost or 8080
+const path = require("path"); //relative windows or MacOS
+
+app.get("/Styles", (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/Styles.css'));
+})
 
 app.get('/', function (req, res) {
-    res.send('This is our main or root page.');
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+    
 });
 
 app.get('/guestbook', function (req, res) {
     res.send('This is our guestbook page.');
 });
+
 
 app.get('/newmessage', function (req, res) {
     res.send('This is our newmessage page.');
